ab0ae1bc03cfda450cf7d7acf3f28db0b94a9443
Arzakan <arzakanporchkhidze17@mgail.com>
2020-11-22 17:24:31 +0400

    Adding input style

--- src/App.js
+++ src/App.js
@@ -15,14 +15,23 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
-    duration: 0
+    duration: 0,
+    animationPercentage: 0
   });
   const [libraryStatus, setLibraryStatus] = useState(false);
   //handlers
   const timeUpdateHandler = (e) => {
-    const current = e.target.currentTime
-    const duration = e.target.duration
-    setSongInfo({...songInfo, currentTime: current, duration: duration})
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const percentage = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo, 
+      currentTime: current, 
+      duration: duration,
+      animationPercentage: percentage,
+    })
   }
   return (
     <div className="App">
